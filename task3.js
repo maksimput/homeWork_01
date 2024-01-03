5aa43cb27182c7c942bd28157df4b21ac9133355
maksimput <maksimput322@yandex.ru>
2024-01-03 15:24:47 +0500

    Add task3. Ways make a function

---
+++ task3.js
@@ -0,0 +1,19 @@
+function makeCounter() {
+return true
+}
+
+const makeCounter1 = function() {
+return true
+}
+
+const makeCounter2 = function makeCounter21() {
+return true
+}
+
+const makeCounter3 = () => {return true}
+
+
+console.log(makeCounter())
+console.log(makeCounter1())
+console.log(makeCounter2())
+console.log(makeCounter3())
